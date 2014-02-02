d2e76e27268746fcf8daa00f558713ca4d5be9ad
Bruno Pinto <brunoferreirapinto@gmail.com>
2014-02-02 00:15:33 -0200

    adding smooth scroll js to navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def navigation_link_to(name = nil, options = nil, html_options = {}, &block)
+    html_options.merge!(data: { scroll: true })
+
+    link_to(name, options, html_options, &block)
+  end
 end
