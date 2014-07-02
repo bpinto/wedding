55c0075932d372d18aa85e1be333a3ee4f085bc7
Bruno Pinto <brunoferreirapinto@gmail.com>
2014-07-02 20:05:55 -0300

    products seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,308 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_LB_LinhaBranca/LGWD1412RT7/LGWD1412RT7_PRD_160_1.jpg',
+  name: 'Lavadora e Secadora de Roupa 10,2 Kg LG Prime Touch Aço Escovado – WD1412RT7',
+  price: 'R$ 2.768,07'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_LA_LinhadeAr/SGCJAQ09UWBUXX/SGCJAQ09UWBUXX_PRD_160_1.jpg',
+  name: 'Ar Condicionado Samsung Split Max Plus 9000Btus',
+  price: 'R$ 1.268,36'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/imagens/_LB_LinhaBranca/BRBRM50NR/BRBRM50NR_PRD_160_1.jpg',
+  name: 'Refrigerador 2 Portas 429L Frost Free Brastemp - BRM50NR',
+  price: 'R$ 2.589,42'
+)
+
+Product.create(
+  image_url: 'http://www.camicado.com.br/images/product/429001.jpg',
+  name: 'Cobertor Microfibra Casal Caqui - Scavone',
+  price: 'R$ 79,90'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_LB_LinhaBranca/BRBVR28HR/V2/BRBVR28HR_PRD_160_1.jpg',
+  name: 'Freezer Frost Free Brastemp Flex Vertical 228L Inox - BVR28HR',
+  price: 'R$ 2.589,42'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/imagens/_LB_LinhaBranca/BRBF475AR/v2/BRBF475AR_PRD_160_1.jpg',
+  name: 'Fogão de Piso 5 Bocas Ative Top Glass Duplo Forno Brastemp - BF475AR',
+  price: 'R$ 3.036,03'
+)
+
+Product.create(
+  image_url: 'http://www.camicado.com.br/images/product/24565.JPG',
+  name: 'Jogo de Colcha Trapuntada Casal 3 peças Safira',
+  price: 'R$ 174,90'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_LB_LinhaBranca/BRBRC12XRANA/BRBRC12XRANA_PRD_160_1.jpg',
+  name: 'Refrigerador Brastemp Compacto 120L Inox - BRC12X',
+  price: 'R$ 1.249,61'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/imagens/_LB_LinhaBranca/BRBLF08AS/BRBLF08AS_PRD_160_1.jpg',
+  name: 'Lava Louça 8 Serviços Ative Brastemp - BLF08AS',
+  price: 'R$ 1.160,28'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/_LB_LinhaBranca/EXMEC52/EXMEC52_PRD_160_1.jpg',
+  name: 'Forno de Micro-ondas Electrolux Home Pro 42L com 10 Níveis de Potência e Painel de Controle Blue Touch Grill Inox - MEC52',
+  price: 'R$ 1.070,96'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/_VD_Video/SOBDPS5100/SOBDPS5100_PRD_160_1.jpg',
+  name: 'Blu-ray Player 3D Sony BDP-S5100',
+  price: 'R$ 498,61'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/SGHTE6750WZD/SGHTE6750WZD_PRD_160_1.jpg',
+  name: 'Home Theater Samsung Blu-Ray 3D com 7.1 Canais, 1330W RMS Preto - HTE6750WZD',
+  price: 'R$ 3.759,96'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/_VD_Video/LG/LG47LA6610/LG47LA6610_PRD_160_1.jpg',
+  name: 'Smart TV LED 3D LG 47" Full HD LG 47LA6610 com Wi-Fi, Entradas HDMI e USB, Controle Smart',
+  price: 'R$ 2.996,18'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_AD_Audio/LGCM2730/V2/LGCM2730_PRD_160_1.jpg',
+  name: 'Micro System LG com Dock Station, Bluetooth, 160W de potência, Entradas para CD e USB, Preto - CM2730',
+  price: 'R$ 799,00'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/_TL_Telefonia/PID2052B/PID2052B_PRD_160_1.jpg',
+  name: 'Telefone sem fio Philips com ramal, DECT 6.0, Preto - D2052B',
+  price: 'R$ 269,30'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/EXACS12_PRD_160_1.jpg',
+  name: 'Adega de Vinhos Electrolux 12 Garrafas, Acabamento em Alumínio Escovado, Preta - ACS12',
+  price: 'R$ 892,27'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/imagens/_UT_Utilidades/EXSUPERCYCLONE/V2/EXSUPERCYCLONE_PRD_160_1.jpg',
+  name: 'Aspirador de Pó Supercyclone 1200W Electrolux',
+  price: 'R$ 704,23'
+)
+
+Product.create(
+  image_url: 'http://img.americanas.com.br/produtos/01/00/item/113022/2/113022290_1GG.jpg',
+  name: 'Batedeira Profissional Philips Walita Viva Multi Velocidades 4 Litros Preto e Inox',
+  price: 'R$ 507,89'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/EXCMC50/EXCMC50_PRD_160_1.jpg',
+  name: 'Cafeteira Chef Electrolux com Timer, Aço Escovado - CMC50',
+  price: 'R$ 252,21'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/ARESPRES/ARESPRES_prd_160_1.jpg',
+  name: 'Máquina de Café Espresso Krups Espresseria Automática - EA8250',
+  price: 'R$ 2.349,62'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/CUCCJ100_PRD_160_1.jpg',
+  name: 'Espremedor de Frutas Cuisinart - CCJ100',
+  price: 'R$ 193,78'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/EXEKC10_PRD_160_1.jpg',
+  name: 'Jarra Elétrica Electrolux Chef 1,5L, Inox EKC10',
+  price: 'R$ 193,78'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/_UT_Utilidades/EXSIP11/V2/EXSIP11_PRD_160_1.jpg',
+  name: 'Ferro a Vapor Electrolux Perfect Line - SIP11',
+  price: 'R$ 253,77'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_UT_Utilidades/EXEOC50/EXEOC50_PRD_160_1.jpg',
+  name: 'Forno Elétrico de Mesa Electrolux Chef 25 L Preto Inox – EOC50',
+  price: 'R$ 381,76'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/WARI2096_0_PRD_160_1.jpg',
+  name: 'Liquidificador Avance Walita 800W de Potência',
+  price: 'R$ 433,39'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/CUCSB_77_PRD_160_1.jpg',
+  name: 'Mixer e Mini Processador Vertical Inox Cuisinart',
+  price: 'R$ 375,15'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/CUDLC1SS_PRD_160_1.jpg',
+  name: 'Mini Processador Cuisinart, em Aço Escovado - DCL1SS',
+  price: 'R$ 252,92'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/EXPA30G_PRD_160_1.jpg',
+  name: 'Purificador de Água Electrolux, Água Natural ou Gelada - PA30G',
+  price: 'R$ 976,89'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/CUWM_SW2_PRD_160_1.jpg',
+  name: 'Sanduicheira Cuisinart - WM_SW2',
+  price: 'R$ 192,08'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/CUCPT160_PRD_160_1.jpg',
+  name: 'Torradeira Elétrica em Aço Escovado Cuisinart',
+  price: 'R$ 412,76'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/AREOLC_PRD_160_1.jpg',
+  name: 'Ventilador Torre Arno, Preto - EOLC',
+  price: 'R$ 365,83'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_US_Utenslios/BRBI904AXONA/BRBI904AXONA_PRD_160_1.jpg',
+  name: 'Kit de Acessórios Brastemp Batedores Manuais Gourmand',
+  price: 'R$ 288,61'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/TR65740553_PRD_160_1.jpg',
+  name: 'Jogo para Fondue em Cerâmica 12 PeçasTramontina',
+  price: 'R$ 715,51'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/_US_Utenslios/MW4080330840/MW4080330840_PRD_160_1.jpg',
+  name: 'Aparelho de Jantar Cashemere Axis, 20 Peças, Maxwell & Williams, Branco, 4080330840',
+  price: 'R$ 610,21'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/TR64510880_PRD_160_1.jpg',
+  name: 'Jogo para Servir 09 peças - Tramontina',
+  price: 'R$ 153,47'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/TR64330100_PRD_160_1.jpg',
+  name: 'Jogo para Servir com 06 Peças - Tramontina',
+  price: 'R$ 124,52'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/TR64210930_PRD_160_1.jpg',
+  name: 'Jogo Térmico para Gelo em Aço Inox Tramontina',
+  price: 'R$ 278,95'
+)
+
+Product.create(
+  image_url: 'http://www.camicado.com.br/images/product/23462.JPG',
+  name: 'Endredom Solteiro Memória 1,7x2,40cm Mult - Altenburg',
+  price: 'R$ 179,90'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/imagens/_LB_LinhaBranca/BRBOG40AR/V2/BRBOG40AR_PRD_160_1.jpg',
+  name: 'Forno Elétrico Brastemp Ative com Capacidade de 40 Litros, Timer, Inox - BOG40AR',
+  price: 'R$ 653,30'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_US_Utenslios/TR23099891/TR23099891_PRD_447_1.jpg',
+  name: 'Conjunto de Facas com Cepo 5 Peças',
+  price: 'R$ 125,62'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/images/catalog/TR20899_450_PRD_447_1.jpg',
+  name: 'Conjunto de Panelas 5 Peças Mônaco Tramontina',
+  price: 'R$ 632,88'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_US_Utenslios/MW4080330524/V2/MW4080330524_PRD_447_1.jpg',
+  name: 'Conjunto de Taças para Champagne Cuveé Maxwell Williams em Vidro Transparente 6 Pçs 160ml',
+  price: 'R$ 106,14'
+)
+
+Product.create(
+  image_url: 'http://prdresources3.fastshop.com.br/wcsstore/FastShopCAS/imagens/_US_Utenslios/CUW050_SACA/V2/CUW050_SACA_PRD_447_1.jpg',
+  name: 'Saca Rolha WO50 Cuisinart',
+  price: 'R$ 234,12'
+)
+
+Product.create(
+  image_url: 'http://prdresources2.fastshop.com.br/wcsstore/FastShopCAS/imagens/_US_Utenslios/TR23799002/TR23799002_PRD_447_1.jpg',
+  name: 'Conjunto de Talheres 14 Peças - Cancun Tramontina',
+  price: 'R$ 77,80'
+)
+
+Product.create(
+  image_url: 'http://prdresources1.fastshop.com.br/wcsstore/FastShopCAS/imagens/_US_Utenslios/TR27899003/TR27899003_PRD_447_1.jpg',
+  name: 'Kit para Pizza 15 peças - Tramontina',
+  price: 'R$ 124,52'
+)
+
+Product.create(
+  image_url: 'http://www.camicado.com.br/images/product/1063.jpg',
+  name: 'Jogo de Assadeiras Marinex Retangular 3 peças - Nadir Figueiredo',
+  price: 'R$ 109,00'
+)
+
+Product.create(
+  image_url: 'http://www.camicado.com.br/images/product/1424.jpg',
+  name: 'Faqueiro Italy 76 peças - Tramontina',
+  price: 'R$ 989,90'
+)
+
+Product.create(
+  image_url: 'http://www.camicado.com.br/images/product/857.jpg',
+  name: 'Suporte para Adoçante e Açúcar Sachê - Brinox',
+  price: 'R$ 52,90'
+)
+
+Product.create(
+  image_url: 'http://www.camicado.com.br/images/product/1854.jpg',
+  name: 'Manteigueira Ciclo - Tramontina',
+  price: 'R$ 89,00'
+)
+
+Product.create(
+  image_url: 'http://www.camicado.com.br/images/product/7602.jpg',
+  name: 'Migalheiro MS Madeiras - Ms Madeiras',
+  price: 'R$ 77,00'
+)
+
+Product.create(
+  image_url: '',
+  name: '',
+  price: ''
+)
