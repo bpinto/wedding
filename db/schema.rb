d2fc48ce92fdd2b0b6036835ebd63f60c4991007
Bruno Pinto <brunoferreirapinto@gmail.com>
2014-07-09 23:23:20 -0300

    add phone number to guests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710013734) do
+ActiveRecord::Schema.define(version: 20140710022200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140710013734) do
     t.datetime "updated_at"
     t.integer  "product_id"
     t.text     "email_message"
+    t.string   "phone_number"
   end
 
   add_index "guests", ["companions"], name: "index_guests_on_companions", using: :gin
